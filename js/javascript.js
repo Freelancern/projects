0cfc863ea509694f8e9eed650430ef6b231c8ea4
Freelancern <70198839+Freelancern@users.noreply.github.com>
2020-09-26 23:55:07 +0600

    personal

---
+++ js/javascript.js
@@ -0,0 +1,44 @@
+
+
+var typed = new Typed('.typing', {
+  strings: [
+  
+  "Web Developer!",
+  "Freelancer!",
+  "Web Designer!"
+  
+  ],
+  typeSpeed: 60,
+  backSpeed: 80,
+  loop: true
+});
+
+$(document).ready(function(){
+    $('#bar1').barfiller({barColor:'#d9832e ', duration:'3000'});
+    $('#bar2').barfiller({barColor:'#d9832e ', duration:'3000'});
+    $('#bar3').barfiller({barColor:'#d9832e ', duration:'3000'});
+    $('#bar4').barfiller({barColor:'#d9832e ', duration:'3000'});
+    $('#bar5').barfiller({barColor:'#d9832e ', duration:'3000'});
+    $('#bar6').barfiller({barColor:'#d9832e ', duration:'3000'});
+});
+
+
+
+// window.onscroll = function() {myFunction()};
+
+// var navbar = document.getElementById("main_menuBar");
+// var sticky = navbar.offSetTop;
+
+// function myFunction() {
+  // if (window.pageYOffset >= sticky) {
+    // navbar.classList.add("sticky")
+  // } else {
+    // navbar.classList.remove("sticky");
+  // }
+// }
+
+
+ // <script src="smooth-scroll/smooth-scroll.js"></script>
+	// <script>
+		// var scroll = new SmoothScroll('a[href*="#"]');
+ // </script>  
